4d443a5008aa64b7a9a355b74b0f987db2846a01
Aftab Sidhu <Aftab180@gmail.com>
2022-03-24 23:04:06 -0400

    commit tree 89d3ac432eddd12e6a1a9c60dd678ab4c1dc088b

---
+++ index.js
@@ -0,0 +1,23 @@
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+const subtract = (num1, num2) => {
+    return num1 - num2;
+}
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+const divide = (num1, num2) => {
+    return num1 / num2;
+}
+
+const increment = (num) => {
+    return num + 1;
+}
+
+const decrement = (num) => {
+    return num - 1;
+}
